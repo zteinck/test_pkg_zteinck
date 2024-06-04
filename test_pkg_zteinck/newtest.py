a0ddd5a8966a125558a0fded717ddf3806d59e57
Zachary Einck <zacharyeinck@gmail.com>
2024-06-03 21:48:35 -0500

    added imports

--- test_pkg_zteinck/newtest.py
+++ test_pkg_zteinck/newtest.py
@@ -1,2 +1,8 @@
-def thisisnew():
-    print('new?')
+from iterlab import to_iter
+import pandas as pd
+import numpy as np
+
+
+def testaroo(x):
+    if pd.isnull(x): return np.nan
+    print(to_iter(x))
